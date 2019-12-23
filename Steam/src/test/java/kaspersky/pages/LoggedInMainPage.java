c127937e1e8876e142e3fa934ca74079890d79e9
Ragimzade <n.ragimzade1@gmail.com>
2019-12-23 10:22:45 +0300

    Add CommonSteps, NavigationBar updates

--- Steam/src/test/java/kaspersky/pages/LoggedInMainPage.java
+++ Steam/src/test/java/kaspersky/pages/LoggedInMainPage.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 public class LoggedInMainPage extends BasePage {
     private static final String NAVIGATION_MENU = "//ul[@class='main-menu js-priority-menu is-priority']//li//a[contains(@href,'%s')]";
     private final Button notificationButton = new Button(By.xpath("//button[@class='w-notifications__bell js-notifications']"), "notificationButton");
+    private final Button accountButton = new Button(By.xpath("(//span[@class='menu-item__text'])[3]"), "accountButton");
+    private final Button signOutButton = new Button(By.xpath("(//span[@class='w-accountMenu__text'])[3]"), "signOutButton");
 
     public LoggedInMainPage() {
         assertPageIsOpened(notificationButton);
@@ -22,5 +24,17 @@ public class LoggedInMainPage extends BasePage {
         new Button(By.xpath(String.format(NAVIGATION_MENU, tab)), String.format("%s Menu Button", tab)).click();
     }
 
+    public void logOut() {
+        accountButton.click();
+        signOutButton.click();
+    }
+
+    public String getUserLogin() {
+        return accountButton.getText();
+    }
+
+    public boolean isAccountButtonPresent() {
+        return accountButton.isButtonOnPage();
+    }
 
 }
