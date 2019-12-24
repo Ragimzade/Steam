448fa2789e94191b57334d148ba04bd00f2ab884
Ragimzade <n.ragimzade1@gmail.com>
2019-12-24 16:48:58 +0300

    Add doLogin fixes

--- Steam/src/test/java/api/configs/ApiConfig.java
+++ Steam/src/test/java/api/configs/ApiConfig.java
@@ -1,12 +1,14 @@
-package api.test;
+package api.configs;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import utils.DateUtil;
 
-public class EndPoint {
-    protected static final String BASE_URL = "http://www.nbrb.by/API/ExRates";
+public class ApiConfig {
+    protected static final String BASE_URI = "http://www.nbrb.by/API/ExRates";
     protected static final String AUTHORIZATION = "Authorization";
     protected static final String CONTENT_TYPE = "Content-Type";
     protected static final String APPLICATION_JSON = "application/json";
@@ -17,11 +19,15 @@ public class EndPoint {
     protected static final String CURRENCIES_FOR_PERIOD = "Rates/Dynamics/145?startDate=2019-1-1&endDate=2019-12-1";
     protected static final String USD_CURRENCY_FOR_TODAY = String.format("Rates/143?onDate=%s", DateUtil.getCurrentDate());
 
-    RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
+    protected RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri(BASE_URI)
             .setAccept(APPLICATION_JSON)
             .setContentType(APPLICATION_JSON)
             .log(LogDetail.ALL)
             .build();
 
+    protected ResponseSpecification responseSpec = new ResponseSpecBuilder()
+            .expectStatusCode(STATUS_CODE_OK)
+            .expectContentType(APPLICATION_JSON)
+            .build();
 }
--- Steam/src/test/java/api/test/ApiTests.java
+++ Steam/src/test/java/api/test/ApiTests.java
@@ -1,12 +1,12 @@
 package api.test;
 
 
+import api.configs.ApiConfig;
 import api.model.CurrencyDescription;
 import api.model.CurrencyRates;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -16,22 +16,19 @@ import java.io.IOException;
 import java.util.Map;
 
 
-public class ApiTests extends EndPoint {
+public class ApiTests extends ApiConfig {
     private Log log = Log.getInstance();
     public static final int EXPECTED_RECORDS_QUANTITY = 225;
 
     @Test
     public void apiTest() throws IOException {
         SoftAssert softAssert = new SoftAssert();
-
         Response listOfCurrenciesResponse = RestAssured.
-                given()
-                .baseUri(BASE_URL)
-                .headers(CONTENT_TYPE, APPLICATION_JSON)
+                given().spec(requestSpec)
                 .when()
                 .get(ALL_CURRENCIES)
                 .then()
-                .statusCode(200).contentType(ContentType.JSON).extract().response();
+                .spec(responseSpec).extract().response();
         int recordsQuantity = listOfCurrenciesResponse.path("Cur_ID.size()");
         softAssert.assertEquals(recordsQuantity, EXPECTED_RECORDS_QUANTITY, "Quantity of records is wrong");
 
@@ -42,41 +39,33 @@ public class ApiTests extends EndPoint {
         CurrencyDescription usdDescription = obj.readValue(json, CurrencyDescription.class);
 
         Response usdDescriptionResponse = RestAssured.
-                given()
-                .baseUri(BASE_URL)
-                .headers(CONTENT_TYPE, APPLICATION_JSON)
+                given().spec(requestSpec)
                 .when()
                 .get(USD_CURRENCY)
                 .then()
-                .statusCode(STATUS_CODE_OK).contentType(APPLICATION_JSON).extract().response();
+                .spec(responseSpec).extract().response();
         CurrencyDescription currencyDescription = usdDescriptionResponse.getBody().as(CurrencyDescription.class);
         softAssert.assertEquals(usdDescription, currencyDescription, "Descriptions are not equal");
 
         Response usdRateForTodayResponse = RestAssured.
-                given()
-                .baseUri(BASE_URL)
-                .headers(CONTENT_TYPE, APPLICATION_JSON)
+                given().spec(requestSpec)
                 .when()
                 .get(USD_CURRENCY_FOR_TODAY)
                 .then()
-                .statusCode(STATUS_CODE_OK).contentType(APPLICATION_JSON).extract().response();
+                .spec(responseSpec).extract().response();
         CurrencyRates usd = usdRateForTodayResponse.getBody().as(CurrencyRates.class);
         log.info(usd.toString());
 
         Response usdRateForPeriodResponse = RestAssured.
-                given()
-                .baseUri(BASE_URL)
-                .headers(CONTENT_TYPE, APPLICATION_JSON)
+                given().spec(requestSpec)
                 .when()
                 .get(CURRENCIES_FOR_PERIOD)
-                .then()
-                .statusCode(STATUS_CODE_OK).contentType(APPLICATION_JSON).extract().response();
+                .then().spec(responseSpec).extract().response();
         String maxRate = usdRateForPeriodResponse.path("max{it.Cur_OfficialRate}.Date");
         log.info(String.format("Max rate was on date %s", maxRate));
         String minRate = usdRateForPeriodResponse.path("min{it.Cur_OfficialRate}.Date");
         log.info(String.format("Min rate was on date %s", minRate));
         softAssert.assertAll();
     }
-
 
 }
